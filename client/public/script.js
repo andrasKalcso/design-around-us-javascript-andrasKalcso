37ef7428a957986332d2515d085b76ecd1f9a725
andrasKalcso <163146864+andrasKalcso@users.noreply.github.com>
2024-05-28 20:18:35 +0200

    project finished

--- client/public/script.js
+++ client/public/script.js
@@ -10,9 +10,61 @@ const loadEvent = function() {
   sectionIDs.forEach(function (e){
     rootE.insertAdjacentHTML("beforeend", sectionElement(e));
   });
-
-
-
 }
 
 window.addEventListener("load", loadEvent);
+
+
+function main(){
+  addClassToClassList();
+  addElementToSections();
+}
+
+window.addEventListener("load", main);
+
+///////////////////////////////////////////////////////
+
+function addClassToClassList(){
+  const sections = document.querySelectorAll('#root section');
+  sections.forEach(function(event){
+    event.classList.add('co');
+  });
+}
+
+///////////////////////////////////////////////////////
+
+/* function addElementToSections() {
+  const sections = document.querySelectorAll('#root section');
+  sections.forEach(function(section, index){
+    const h2Node = document.createElement('h2');
+    const h2TextNode = document.createTextNode(sectionH2texts[index]);
+    h2Node.appendChild(h2TextNode);
+  
+    section.appendChild(h2Node);
+  });
+} */
+
+
+/* function addElementToSections() {
+  const sections = document.querySelectorAll("#root section");
+  sections.forEach(function (e,i){
+    e.classList.add("co");
+
+    e.insertAdjacentHTML("beforeend", `<h2>${sectionH2texts[i]}</h2>`)
+  });
+} */
+
+function addElementToSections() {
+  const sections = document.querySelectorAll('#root section');
+  sections.forEach(function(section, index){
+    const h2Node = document.createElement('h2');
+    const h2TextNode = document.createTextNode(sectionH2texts[index]);
+    h2Node.appendChild(h2TextNode);
+
+    const sectionId = sectionIDs[index];
+    document.querySelector(`#${sectionId}`).appendChild(h2Node);
+  });
+}
+
+///////////////////////////////////////////////////////////
+
